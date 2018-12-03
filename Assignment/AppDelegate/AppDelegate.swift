433f6a8d63eb6dc89b0340b1b77d781b977d1eb5
Rohit Sharma <rohit0690@gmail.com>
2018-12-04 10:53:47 +1100

    F1

--- Assignment/AppDelegate/AppDelegate.swift
+++ Assignment/AppDelegate/AppDelegate.swift
@@ -5,23 +5,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+	// application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
+	// applicationWillResignActive
     func applicationWillResignActive(_ application: UIApplication) {
     }
 
+	// applicationDidEnterBackground
     func applicationDidEnterBackground(_ application: UIApplication) {
     }
 
+	// applicationWillEnterForeground
     func applicationWillEnterForeground(_ application: UIApplication) {
     }
 
+	// applicationDidBecomeActive
     func applicationDidBecomeActive(_ application: UIApplication) {
     }
 
+	// applicationWillTerminate
     func applicationWillTerminate(_ application: UIApplication) {
     }
 }
